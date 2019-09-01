d35f563ddfa02b9d3c013117e62a484a99e14ffb
Maksim Skutin <skutin@protonmail.com>
2019-09-01 20:37:18 +0000

    Created data Ingester service

---
+++ ingester/src/ingester.js
@@ -0,0 +1,76 @@
+const { PubSub } = require('@google-cloud/pubsub')
+const ps = new PubSub()
+const db = require('./config/db')
+const TripModel = require('./models/trip')
+const config = require('./config/config')
+
+const { pubsub: { maxInProgress } } = config
+
+const subscriberOptions = {
+  flowControl: {
+    maxMessages: maxInProgress,
+  }
+}
+const topic = 'projects/pubsub-public-data/topics/taxirides-realtime'
+const subscriptionName = 'trips'
+
+run()
+
+async function run() {
+  const createSubscription = async () => {
+    const matchSubscriptionAndTopic = (s) => {
+      if (s.length === 0) { return false }
+      console.log
+      return s[0].name.split("/").reverse()[0] === subscriptionName &&
+             s[0].metadata.topic === topic
+    }
+
+    const [subscriptions] = await ps.getSubscriptions()
+
+    if (!(matchSubscriptionAndTopic(subscriptions))) {
+      const subscription = await ps.topic(topic).createSubscription(subscriptionName)
+      console.log(`Subscription ${subscriptionName} created.`)
+    }
+  }
+  await createSubscription()
+  const listener = ps.subscription(subscriptionName, subscriberOptions)
+  console.info(`Subscriber to subscription ${subscriptionName} is ready to receive messages at a controlled volume of ${maxInProgress} messages.`)
+
+  const saveToDB = async (document) => {
+    await TripModel.collection.insertMany([document])
+  }
+  const mapMessageToModel = (message) => {
+    return {
+      ride_id: message.ride_id,
+      point_idx: message.point_idx,
+      latitude: message.latitude,
+      longitude: message.longitude,
+      timestamp: new Date(message.timestamp).toISOString(),
+      meter_reading: message.meter_reading,
+      meter_increment: message.meter_increment,
+      ride_status: message.ride_status,
+      passenger_count: message.passenger_count
+    }
+  }
+  let messageCount = 0
+
+  const messageHandler = async (message) => {
+    try {
+      const decoded = JSON.parse(Buffer.from(message.data, 'base64').toString('ascii'))
+      const mapped = mapMessageToModel(decoded)
+      console.log(mapped)
+      await saveToDB(mapped)
+      messageCount += 1
+      message.ack()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const errorHandler = error => {
+    console.error(error)
+  }
+
+  listener.on(`message`, messageHandler)
+  listener.on(`error`, errorHandler)
+}
